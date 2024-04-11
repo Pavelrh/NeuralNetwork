404c894ff59b55c09851a6be026fd69c3bbe186b
Pavelrh <shridharapavel@gmail.com>
2024-04-10 23:07:27 -0400

    started using oop for layer

--- main.py
+++ main.py
@@ -4,27 +4,8 @@ inputs = [[1.7, 2.3, 10.7],
           [2.7, 4.3, 0.7],
           [3.7, 6.3, -10.7]]
 
-weights =  [[4, 2, -6.3],
-            [-4.3, 2.4, 3],
-            [6.3, 3.4, -6.3]]
-
-biases = [1,
-          2,
-          3]
-
-weights2 =  [[3, 1, -5.3],
-            [-3.3, 1.4, 2],
-            [5.3, 1.4, -5.3]]
-
-biases2 = [4,
-          5,
-          6]
-
-layer1_output = numpy.dot(inputs, numpy.array(weights).T) + biases
-
-layer2_output = numpy.dot(layer1_output, numpy.array(weights2).T) + biases2
-
-
-
-print(layer1_output)
-print(layer2_output)
+class layer_obj:
+    def __init__(self):
+         pass
+    def forward(self):
+         pass
