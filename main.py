82c2dea9a790e6d1bbab1aacce568260c1b516e0
Pavelrh <shridharapavel@gmail.com>
2024-04-10 23:17:31 -0400

    made it initalise biases using .zeros()

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ class layer_obj:
     def __init__(self, n_inputs, n_neurons):
          # its gotta be this way cus for matrix multiplication the no of rows in matrix 1 should be equal to no of columns in matrx 2
          self.weights = numpy.random.randn(n_inputs, n_neurons)
+         self.weights = numpy.zeros(1, n_neurons)
     def forward(self):
          pass
     
