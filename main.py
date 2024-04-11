f29c63a4758e6cf1cdf031a630ff6e02782e0b5e
Pavelrh <shridharapavel@gmail.com>
2024-04-10 23:20:00 -0400

    finished the forawrd function

--- main.py
+++ main.py
@@ -2,15 +2,15 @@ import numpy
 
 numpy.random.seed(0)
 
-inputs = [[1.7, 2.3, 10.7],
+X = [[1.7, 2.3, 10.7],
           [2.7, 4.3, 0.7],
           [3.7, 6.3, -10.7]]
 
-class layer_obj:
+class layer:
     def __init__(self, n_inputs, n_neurons):
          # its gotta be this way cus for matrix multiplication the no of rows in matrix 1 should be equal to no of columns in matrx 2
          self.weights = numpy.random.randn(n_inputs, n_neurons)
-         self.weights = numpy.zeros(1, n_neurons)
-    def forward(self):
-         pass
+         self.biases = numpy.zeros(1, n_neurons)
+    def forward(self, n_inputs):
+         self.output = self.weights * n_inputs + self.biases
     
