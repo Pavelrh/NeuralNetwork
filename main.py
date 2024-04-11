1a610cf6b9e0efdc6ffc80ae08f5892324a1aaf9
Pavelrh <shridharapavel@gmail.com>
2024-04-10 23:29:22 -0400

    initialised layer objects and used the forward function to make it work

--- main.py
+++ main.py
@@ -10,7 +10,13 @@ class layer:
     def __init__(self, n_inputs, n_neurons):
          # its gotta be this way cus for matrix multiplication the no of rows in matrix 1 should be equal to no of columns in matrx 2
          self.weights = numpy.random.randn(n_inputs, n_neurons)
-         self.biases = numpy.zeros(1, n_neurons)
-    def forward(self, n_inputs):
-         self.output = self.weights * n_inputs + self.biases
+         self.biases = numpy.zeros((1, n_neurons))
+    def forward(self, inputs):
+         self.output = numpy.dot(inputs, self.weights) + self.biases
     
+layer1 = layer(3, 6)
+layer2 = layer(6, 2)
+
+layer1.forward(X)
+layer2.forward(layer1.output)
+print(layer2.output)
