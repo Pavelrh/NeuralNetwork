647f3e57ebda92aa1c027646d35ce56a3f0c6e8f
Pavelrh <shridharapavel@gmail.com>
2024-04-10 23:16:14 -0400

    made it generate weights using .randn()

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import numpy 
 
+numpy.random.seed(0)
+
 inputs = [[1.7, 2.3, 10.7],
           [2.7, 4.3, 0.7],
           [3.7, 6.3, -10.7]]
 
 class layer_obj:
-    def __init__(self):
-         pass
+    def __init__(self, n_inputs, n_neurons):
+         # its gotta be this way cus for matrix multiplication the no of rows in matrix 1 should be equal to no of columns in matrx 2
+         self.weights = numpy.random.randn(n_inputs, n_neurons)
     def forward(self):
          pass
+    
